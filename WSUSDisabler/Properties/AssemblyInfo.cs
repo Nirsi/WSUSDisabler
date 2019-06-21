b93b878e90497c9bfa42c9d46e608afd4f705184
Jan Novak <novak@silen.org>
2019-06-21 11:53:14 +0200

    Assembly info changes

--- WSUSDisabler/Properties/AssemblyInfo.cs
+++ WSUSDisabler/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WSUSDisabler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Simple application to disable WSUS as source of updates for computer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("silen.org")]
 [assembly: AssemblyProduct("WSUSDisabler")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Jan Novák | GPLv3")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyVersion("")]
+//[assembly: AssemblyFileVersion("")]
