1b569c379e13109177cbe7f855f9260a927a99cd
Jan Novak <novak@silen.org>
2019-06-25 14:52:25 +0200

    Work in progress but working registry class
    Reworked Registry class, mostly working but contains a lot of old code.

--- WSUSDisabler/RegistryEditor.cs
+++ WSUSDisabler/RegistryEditor.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Security.AccessControl;
+using System.Windows.Forms;
 using Microsoft.Win32;
 
 namespace WSUSDisabler
@@ -7,18 +8,25 @@ namespace WSUSDisabler
     public class RegistryEditor
     {
         private readonly RegistryKey _registryKey;
+        private RegistryKey _backupKey;
+        private RegistryKey _workKey;
+
+        private const string MasterKey = @"SOFTWARE\MyTest";
+        private const string WorkKey = @"Unit";
+        private const string BackupName = @"BackUp";
 
         public RegistryEditor()
         {
-            _registryKey =
-                Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU", true);
+            //_registryKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", true);
+            _registryKey = Registry.CurrentUser.OpenSubKey(MasterKey, true);
+
         }
 
         public bool IsWsusUp()
         {
             return _registryKey != null;
         }
-
+        
         public int GetKeyValue()
         {
             return Convert.ToInt32(_registryKey.GetValue("UseWUServer"));
@@ -36,11 +44,62 @@ namespace WSUSDisabler
             _registryKey.SetValue("UseWUServer", "0", RegistryValueKind.DWord);
         }
         
-        */
+        
 
         public void SwitchWsus(int value)
         {
             _registryKey.SetValue("UseWUServer", value.ToString(), RegistryValueKind.DWord);
+
+            _registryKey.Flush();
+        }
+        
+        */
+
+        public void BackupCleanUp()
+        {
+            if (_backupKey != null)
+            {
+                _backupKey.Dispose();
+                _registryKey.DeleteSubKeyTree(BackupName);
+            }
+        }
+
+        public void MakeBackup()
+        {
+
+            //TODO: Need refactoring!
+            _registryKey.CreateSubKey(BackupName);
+            _backupKey = _registryKey.OpenSubKey(BackupName, true);
+            _workKey = _registryKey.OpenSubKey(WorkKey, true);
+
+            foreach (var valueName in  _registryKey.OpenSubKey(WorkKey).GetValueNames())
+            {
+                var value = _workKey.GetValue(valueName);
+                var valueKind = _workKey.GetValueKind(valueName);
+                _backupKey.SetValue(valueName, value, valueKind);
+            }
+        }
+
+        public void RemoveWorkKey()
+        {
+            _registryKey.DeleteSubKeyTree(WorkKey);
+        }
+
+        public void RestoreWorkKey()
+        {
+            _registryKey.CreateSubKey(WorkKey);
+            var _workKey = _registryKey.OpenSubKey(WorkKey, true);
+            foreach (var valueName in _registryKey.OpenSubKey(BackupName).GetValueNames())
+            {
+                var value = _backupKey.GetValue(valueName);
+                var valueKind = _backupKey.GetValueKind(valueName);
+                _workKey.SetValue(valueName, value, valueKind);
+            }
+        }
+
+        public bool WsusCheckState()
+        {
+            return _registryKey.OpenSubKey(WorkKey) != null;
         }
     }
 }
